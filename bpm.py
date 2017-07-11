abc0b2dfaa57737d2c3111a001fe761ba2b6736d
Mary Ellen Weber <mmweber2@gmail.com>
2017-07-11 14:33:25 -0500

    Add MAX_BPM_VARIANCE.
    Add check to see if the BPM difference exceeds MAX_BPM_VARIANCE.
    Create VariedBPMException class.

--- bpm.py
+++ bpm.py
@@ -1,23 +1,27 @@
 import sys
 
-
 def avg(nums):
     return sum(nums) / len(nums)
 
-# TODO: Determine when a song varies too much to calculate
+class VariedBPMException(Exception):
+    pass    
 
-lines = []
+# The maximum difference in BPMs to allow before considering a song
+#     too irregular
+MAX_BPM_VARIANCE = 1
+    
+file_lines = []
 
 with open(sys.argv[1], 'r') as filename:
-    lines = filename.readlines()
-beats = [float(x) for x in lines]
+    file_lines = filename.readlines()
+beats = [float(x) for x in file_lines]
 beat_count = len(beats)
-print "Beat count is ", beat_count
+#print "Beat count is", beat_count
 
 # Try subtracting first beat to get a more accurate average
 # Any time in the song after the last beat is also skipped
 song_secs = beats[-1] - beats[0]
-print "Song is {} seconds long".format(song_secs)
+#print "Song is {} seconds long".format(song_secs)
 
 # Try tracking all beat differences and average of every 4 to balance out spikes
 beat_diffs = []
@@ -27,22 +31,27 @@ for i in xrange(1, len(beats)):
     if i % 4 == 0:
         avg_beat_diffs.append(sorted(beat_diffs[i-3:i+1])[1])
         
-print "Average of all differences is ", avg(beat_diffs)
-print "Average of average differences is ", avg(avg_beat_diffs)
+beat_diffs = sorted(beat_diffs)  
+avg_beat_diffs = sorted(avg_beat_diffs)      
+print "Median difference of averages is: ", avg_beat_diffs[len(avg_beat_diffs) / 2]
+print "Median difference of all beats is: ", beat_diffs[len(beat_diffs) / 2]
 
-print "Median difference is: ", sorted(avg_beat_diffs)[len(avg_beat_diffs) / 2]
+# Discard songs with large variance in BPM
+max_diff = beat_diffs[-1] - beat_diffs[0]
+if max_diff > MAX_BPM_VARIANCE:
+    raise VariedBPMException("Song {} varies by {} BPM.".format(arg[1], max_diff))
 
-num_diffs = len(avg_beat_diffs)
-med_avg = avg(avg_beat_diffs[num_diffs / 3: num_diffs - (num_diffs / 3)])
+med_avg = avg(avg_beat_diffs)
 overall_avg = avg(beat_diffs)
 print "Average of median beat diffs is ", med_avg
 print "Average of all beat diffs is ", overall_avg
 
 beats_sec = 1 / med_avg
-#beats_sec = 1 / overall_avg
 
 print "Beats/sec is ", beats_sec
 
-print "That's {} bpm.".format(beats_sec * 60)
+print "From the median average, that's {} bpm.".format(beats_sec * 60)
+print "From the overall average, that's {} bpm.".format(1 / overall_avg * 60)
+print "Average of these two is", avg((beats_sec * 60, 1 / overall_avg * 60))
 
 

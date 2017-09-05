6602954155f58062fb16ec33f5011188b7edca5b
Mary Ellen Weber <mmweber2@gmail.com>
2017-09-05 12:12:40 -0500

    Add documentation.

--- bpm.py
+++ bpm.py
@@ -1,22 +1,34 @@
 import sys
 
+# Usage: python bpm.py (input)
+# where (input) is a stdin list of beats
+
+# TODO: Aim for accuracy of 1/60 second (within 0.05 BPM)
 def avg(nums):
     return sum(nums) / len(nums)
 
 class VariedBPMException(Exception):
-    pass    
+    pass
 
 # The maximum difference in BPMs to allow before considering a song
 #     too irregular
 MAX_BPM_VARIANCE = 1
-    
-file_lines = []
 
-with open(sys.argv[1], 'r') as filename:
-    file_lines = filename.readlines()
-beats = [float(x) for x in file_lines]
+#file_lines = []
+
+#with open(sys.argv[1], 'r') as filename:
+    #file_lines = filename.readlines()
+beat = raw_input()
+beats = []
+while beat is not None:
+    beats.append(float(beat))
+    try:
+        beat = raw_input()
+    except EOFError:
+        break
+#beats = [float(x) for x in file_lines]
 beat_count = len(beats)
-#print "Beat count is", beat_count
+print "Beat count is", beat_count
 
 # Try subtracting first beat to get a more accurate average
 # Any time in the song after the last beat is also skipped
@@ -30,9 +42,9 @@ for i in xrange(1, len(beats)):
     beat_diffs.append(beats[i] - beats[i-1])
     if i % 4 == 0:
         avg_beat_diffs.append(sorted(beat_diffs[i-3:i+1])[1])
-        
-beat_diffs = sorted(beat_diffs)  
-avg_beat_diffs = sorted(avg_beat_diffs)      
+
+beat_diffs = sorted(beat_diffs)
+avg_beat_diffs = sorted(avg_beat_diffs)
 print "Median difference of averages is: ", avg_beat_diffs[len(avg_beat_diffs) / 2]
 print "Median difference of all beats is: ", beat_diffs[len(beat_diffs) / 2]
 
@@ -41,14 +53,17 @@ max_diff = beat_diffs[-1] - beat_diffs[0]
 if max_diff > MAX_BPM_VARIANCE:
     raise VariedBPMException("Song {} varies by {} BPM.".format(arg[1], max_diff))
 
-med_avg = avg(avg_beat_diffs)
-overall_avg = avg(beat_diffs)
-print "Average of median beat diffs is ", med_avg
+# TODO: Median of averages is the same as median of everything,
+# how can we bring this closer?
+overall_avg = beat_diffs[len(beat_diffs) / 2]
+merged_avg = avg_beat_diffs[len(avg_beat_diffs) / 2]
+
+print "Average of median beat diffs is ", merged_avg
 print "Average of all beat diffs is ", overall_avg
 
-beats_sec = 1 / med_avg
+beats_sec = 1 / merged_avg
 
-print "Beats/sec is ", beats_sec
+print "Beats/sec from median average is ", beats_sec
 
 print "From the median average, that's {} bpm.".format(beats_sec * 60)
 print "From the overall average, that's {} bpm.".format(1 / overall_avg * 60)
